0eadcaea07f1cfc14d697ea24a23c41b4258fef3
GabrielePrestifilippo <gabry.501@live.it>
2017-01-19 12:23:32 +0100

    Refactor server classes and new POM to use last Spring

--- server/src/main/java/server/Application.java
+++ server/src/main/java/server/Application.java
@@ -7,8 +7,8 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
-
     public static void main(String[] args) {
         org.springframework.boot.SpringApplication.run(Application.class, args);
     }
+
 }
